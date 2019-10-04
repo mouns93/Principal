c90acbd8e35938beb90941d198212c4f7029c6b2
Mouns <mounir.aiis@gmail.com>
2019-10-04 22:53:29 +0200

    essai d optimisation

--- ZCasino.py
+++ ZCasino.py
@@ -4,9 +4,11 @@ import math
 debut = True
 while debut:
     wallet = input("De combien nous créditons votre porte-feuille :\n")
+    
     if wallet.isdigit():
         wallet = int(wallet)
         debut = False
+    
     else :
         print ("ce n'est pas une somme !")
 
@@ -15,21 +17,31 @@ parti = "o"
 
 while not parti in "nN" :
     choice_number = input("Sur quel numéro miser entre 0 et 49 ? \n")
+    if not choice_number.isdigit() :
+        print("Entrer un numero svp entre 0 et 49")
+        continue
+    choice_number = int (choice_number)
+    if not (choice_number in range (50)) :
+        print("il doit etre compris entre 0 et 49")
+        continue
     mise = input("Quel somme miser dessus ? \n")
-    if choice_number.isdigit() and mise.isdigit() :
-        choice_number = int (choice_number)
-        mise = int (mise)
-        if choice_number in range (50) :
+    if not mise.isdigit() :
+        print("Entrer que des nombres svp ")
+        continue
+    mise = int (mise)
+    win_number = random.randrange(50)
+
+    if choice_number in range (50) :
+            
             if choice_number == win_number :
                 mise *= 3
                 wallet += mise 
-                win_number = random.randrange(50)
                 print(f"Jackpot , vous avez gagné {mise}$ votre porte-feuille est de {wallet}$")
                 parti = input("Rejouer ? (o/n) : ")
+            
             elif (choice_number % 2 == 0 and win_number % 2 == 0) or (choice_number % 2 == 1 and win_number % 2 == 1):
                 mise += math.ceil(mise/2)
                 wallet += mise 
-                win_number = random.randrange(50)
                 print(f"Bravo les 2 numero sont de la meme couleur, vous avez gagné {mise}$ votre porte-feuille est de {wallet}$")
                 parti = input("Rejouer ? (o/n) : ")
                 
@@ -37,13 +49,9 @@ while not parti in "nN" :
                 wallet -= mise
                 print (f"Dommage , votre porte-feuille est à {wallet}$ ")
                 parti = input("Rejouer ? (o/n) : ")
-                win_number = random.randrange(50)
+                
         else :
             print("Entrer un numero svp entre 0 et 49")
-    
-
-    else :
-        print("Entrer que des nombres svp ")
 
 if wallet < 0 :
     print (f"merci d'avoir jouer votre porte-feuille est negatif vous nous devez {wallet}$ ")
